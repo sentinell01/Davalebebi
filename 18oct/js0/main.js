d34c43fe7c6b49f9d28ecee8c9db04680c2921fc
Donki Xoti <example@gmail.com>
2019-10-18 11:42:01 +0400

    js0

---
+++ 18oct/js0/main.js
@@ -0,0 +1,23 @@
+let TheGuy = prompt("Who is dat?") 
+if (TheGuy == "Admin") {
+    let Pass = prompt("Enter Your Password Kiddo!")
+    if (Pass == "TheMaster") {
+        alert(" There u go lil buddy ;) ")
+    }
+    else if (pass == "") {
+        alert("Okey... Whatever... ;)")
+    }
+    else {
+        alert ("Davai Nafig exla ")
+    }
+} 
+else if (TheGuy == "") {
+    alert("Okey... Whatever...")
+}
+
+else {
+    alert ("Nafig")
+}
+
+
+
